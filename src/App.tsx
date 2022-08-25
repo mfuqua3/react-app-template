9d6795c643576e869ea32b2edcb2b6784b02edac
Matt Fuqua <fuqua.matt@gmail.com>
2022-08-25 15:07:18 -0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AuthProvider from "./utils/auth/AuthProvider";
+import useOidcEnvironmentConfiguration from "./utils/hooks/useOidcEnvironmentConfiguration";
+import AppRoutes from "./App.Routes";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const oidcEnvironmentConfig = useOidcEnvironmentConfiguration();
+    return (
+        <AuthProvider {...oidcEnvironmentConfig}>
+            <AppRoutes />
+        </AuthProvider>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import {createRoot} from 'react-dom/client';
+import {BrowserRouter} from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import {CssBaseline, ThemeProvider} from "@mui/material";
+import {defaultTheme} from "./themes/defaultTheme";
+import AdapterMoment from "@date-io/moment";
+import {SnackbarProvider} from "./utils/snackbar";
+import ModalProvider from "./utils/modal/ModalProvider";
+import {LocalizationProvider} from "@mui/x-date-pickers";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href");
+const rootElement = document.getElementById("root");
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(rootElement!);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <LocalizationProvider dateAdapter={AdapterMoment}>
+            <ThemeProvider theme={defaultTheme}>
+                <CssBaseline/>
+                <BrowserRouter basename={baseUrl ?? undefined}>
+                    <SnackbarProvider>
+                        <ModalProvider>
+                            <App/>
+                        </ModalProvider>
+                    </SnackbarProvider>
+                </BrowserRouter>
+            </ThemeProvider>
+        </LocalizationProvider>
+    </React.StrictMode>
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
